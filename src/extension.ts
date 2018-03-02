b1b6504864600c44c9b4d39a0c30a1c3d765b088
kyle <kingerkyleking@gmail.com>
2018-03-01 21:24:03 -0700

    Structuring project.

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,33 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "npm-version-util" is now active!');
+    console.log('NPM version utility loaded.');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    const currentEditor: vscode.TextEditor = vscode.window.activeTextEditor;
+    const darkBackground: string = "rgba(60, 60, 60, .8)";
+    const lightBackground: string = "rgba(155, 155, 155, .8)";
+    
+    const decoration: vscode.TextEditorDecorationType = vscode.window.createTextEditorDecorationType({
+        rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
+        dark: {
+            after: {
+                backgroundColor: "rgba(48,48,48,.8)",
+                color: "rgba(155, 155, 155, .8)",
+            }
+        },
+        after: {
+            contentText: "Hello world :)",
+            textDecoration: 'margin: 0 6px 0 6px; padding: 0 6px 0 6px; border-radius: 6px;',
+            color: "rgba(0, 0, 0, .5)",
+            backgroundColor: "rgba(200, 200, 200, 1)",
+        }
+    });    
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    currentEditor.setDecorations(decoration, [new vscode.Range(new vscode.Position(1, 1), new vscode.Position(1, 1))]);
 }
 
-// this method is called when your extension is deactivated
+
 export function deactivate() {
 }
